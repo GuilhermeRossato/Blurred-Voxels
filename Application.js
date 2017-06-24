c9f83c91037e570b3ba12334fcd8083fc1a09142
Guilherme Rossato <guilhermrossato@gmail.com>
2017-06-24 05:04:11 -0300

    Added Performancer on FixedUpdate function

--- Application.js
+++ Application.js
@@ -2,22 +2,32 @@ const Application = (function() {
 	var performancer = new Performancer();
 	performancer.wrapper.style.zIndex = "11";
 	//performancer.wrapper.style.display = "none";
-	var world;
+	var world, cinematic;
 	function firstFrame() {
 		lastTimeStamp = 0;
 		leftOver = 0;
 		world = new WorldHandler(this.images);
 		world.init(GUI.camera, GUI.scenes);
+		cinematic = new Cinematic();
+		cinematic.init(world, GUI.camera, GUI.scenes);
+		this.world = world;
 		GUI.render();
 		GUI.renderers.forEach(render=>render.domElement.style.display = "block");
-		if (true)
+		if (false)
 			window.requestAnimationFrame(update);
 		else
 			window.requestAnimationFrame(fixedUpdate);
 	}
 	var difference, timeStamp, lastTimeStamp = 0, leftOver = 0;
 	function fixedUpdate() {
-		world.update(world.animationPeriod/180);
+		timeStamp = performance.now();
+		difference = timeStamp - lastTimeStamp;
+		performancer.update(difference);
+		lastTimeStamp = timeStamp;
+		
+		difference = cinematic.periods.animation/360;
+		cinematic.update(difference);
+		world.update(difference);
 		GUI.render();
 		window.requestAnimationFrame(fixedUpdate);
 	}
@@ -25,8 +35,10 @@ const Application = (function() {
 		timeStamp = performance.now();
 		difference = timeStamp - lastTimeStamp;
 		performancer.update(difference);
-		world.update(difference/16);
 		lastTimeStamp = timeStamp;
+		difference /= 16;
+		cinematic.update(difference);
+		world.update(difference);
 		GUI.render();
 		window.requestAnimationFrame(update);
 	}
@@ -38,9 +50,13 @@ const Application = (function() {
 		startMainLoop: function() {
 			firstFrame.call(this);
 		},
+		addChunk: function(chunk) {
+			cinematic.onChunkAdded();
+			return world.addChunk(chunk);
+		},
 		onKeyDown: function(event) {
 			if (event.code === "KeyS")
-				singleUpdate();
+				fixedUpdate();
 			if (event.code === "KeyR")
 				world.reset();
 		}
