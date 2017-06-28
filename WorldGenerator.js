57ced9625715208e526a9d8f3e67403f9df475f8
Guilherme Rossato <guilhermrossato@gmail.com>
2017-06-28 00:31:47 -0300

    Added sorting to chunks
    So that chunks start from the closest to the origin

--- WorldGenerator.js
+++ WorldGenerator.js
@@ -9,6 +9,8 @@ function WorldGenerator(x, z, callback) {
 	} else {
 		offsets.push([0,1,1], [0,1,-1], [0,1,0], [1,1,1], [1,1,-1], [1,1,0],[-1,1,1], [-1,1,-1], [-1,1,0]);
 		offsets.push([0,0,1], [0,0,-1], [0,0,0], [1,0,1], [1,0,-1], [1,0,0],[-1,0,1], [-1,0,-1], [-1,0,0]);
+		offsets.sort((a,b) => ((a[0]*a[0]+a[1]*a[1]+a[2]*a[2] > b[0]*b[0]+b[1]+b[1]+b[2]*b[2])?-1:1));
+		console.log("offsets = "+JSON.stringify(offsets));
 	}
 	offsets.last = function() { return offsets[offsets.length-1]; }
 	this.events = {
@@ -21,8 +23,9 @@ function WorldGenerator(x, z, callback) {
 		if (offsets.length > 0) {
 			var offset = offsets.pop();
 			var worldData = new WorldDataArray(chunkSize, chunkSize, chunkSize);
+			var count = ev.data.substr(1,ev.data.indexOf(",")-1);
 			worldData.decode(ev.data.substr(ev.data.indexOf(",")+1));
-			ww.chunks.push({data: worldData, offset: offset});
+			ww.chunks.push({data: worldData, offset: offset, count: parseInt(count)});
 			if (offsets.length > 0) {
 				var lastOffset = offsets.last();
 				var relativeX = origin.x+lastOffset[0];
